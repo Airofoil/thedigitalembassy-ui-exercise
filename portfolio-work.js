f1ecfccde0bdc0edd73982ee3e53c3721aca3912
Airofoil <68868228+Airofoil@users.noreply.github.com>
2020-10-02 09:41:01 +0930

    Added .html, .css, assets, and images

---
+++ portfolio-work.js
@@ -0,0 +1,41 @@
+[{
+    "icon": "item-1.png",
+    "name": "Aliquam Risus",
+    "info": "Donec purus diam, convallis eget turpis"
+},{
+    "icon": "item-1.png",
+    "name": "Vivamus Fringilla",
+    "info": "Donec purus diam, convallis eget turpis"
+},{
+    "icon": "item-1.png",
+    "name": "Praesent Bibendum",
+    "info": "Donec purus diam, convallis eget turpis"
+},{
+    "icon": "item-1.png",
+    "name": "Vehicula Tortor",
+    "info": "Donec purus diam, convallis eget turpis"
+},{
+    "icon": "item-1.png",
+    "name": "Maecenas Imperdiet",
+    "info": "Donec purus diam, convallis eget turpis"
+},{
+    "icon": "item-1.png",
+    "name": "Aliquam Risus",
+    "info": "Donec purus diam, convallis eget turpis"
+},{
+    "icon": "item-1.png",
+    "name": "Vivamus Fringilla",
+    "info": "Donec purus diam, convallis eget turpis"
+},{
+    "icon": "item-1.png",
+    "name": "Praesent Bibendum",
+    "info": "Donec purus diam, convallis eget turpis"
+},{
+    "icon": "item-1.png",
+    "name": "Vehicula Tortor",
+    "info": "Donec purus diam, convallis eget turpis"
+},{
+    "icon": "item-1.png",
+    "name": "Maecenas Imperdiet",
+    "info": "Donec purus diam, convallis eget turpis"
+}]
